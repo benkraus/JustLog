ba7684da7cc328834e2b71fb82ee42509b9e195b
Ben Kraus <ben@practicalbyte.com>
2018-03-06 09:46:01 -0700

    Carthage support

---
+++ JustLog/JustLog.h
@@ -0,0 +1,19 @@
+//
+//  JustLog.h
+//  JustLog
+//
+//  Created by Ben Kraus on 3/6/18.
+//  Copyright © 2018 Practical Byte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JustLog.
+FOUNDATION_EXPORT double JustLogVersionNumber;
+
+//! Project version string for JustLog.
+FOUNDATION_EXPORT const unsigned char JustLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustLog/PublicHeader.h>
+
+
